731279c3528075f2bd8ecb6db3c5d6cccd24ee76
Salome-SM <solanomiguelsalome@gmail.com>
2025-02-24 19:57:46 -0500

    Delete plant_counter directory

--- plant_counter/config/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'host' => getenv('DB_HOST') ?: 'localhost',
-    'dbname' => getenv('DB_NAME') ?: 'plant_counter',
-    'username' => getenv('DB_USER') ?: 'postgres',
-    'password' => getenv('DB_PASS') ?: 'password',
-    'port' => getenv('DB_PORT') ?: '5432'
-];
